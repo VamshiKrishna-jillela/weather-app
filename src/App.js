68104244d70448c93b80e5aa4e07f4247d6d5054
vamshi-krishna-jillela <vamshikrishnareddy2550@gmail.com>
2023-05-31 15:31:25 +0530

    final one

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
-function App() {
+export default function App() {
+  const [place, setPlace] = useState();
+  const [submit, setSubmit] = useState(0);
+  const [output, setOutput] = useState("Try Something...");
+
+  useEffect(() => {
+    if (!place) return;
+    let z = `http://api.weatherstack.com/current?access_key=${process.env.REACT_APP_SECRET}&query=${place}`;
+    console.log(z);
+    fetch(z)
+      .then((response) => response.json())
+      .then((data) => {
+        if (!data["current"]) {
+          setOutput("Entered Location is Out of Range of Server...");
+        } else {
+          setOutput(
+            `It's Currently ${data["current"]["temperature"]} degrees out and there is ${data["current"]["humidity"]}% Humidity in  ${data["location"]["name"]}, ${data["location"]["region"]}, ${data["location"]["country"]} with latitude ${data["location"]["lat"]} and longitude ${data["location"]["lon"]}.`
+          );
+        }
+        console.log(data);
+      })
+      .catch((err) => console.log(err));
+  }, [submit]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        margin: "0 auto",
+        padding: "20px",
+        textAlign: "center",
+        fontSize: "20px",
+      }}
+      className="App"
+    >
+      <h1>Weather App</h1>
+      <input
+        type="text"
+        placeholder="Location"
+        onChange={(event) => setPlace(event.target.value)}
+      />
+
+      <button
+        onClick={() => {
+          if (place) {
+            setOutput("Fetching....");
+            setSubmit(1 - submit);
+          } else setOutput("Please!!! Enter Valid Location");
+        }}
+        type="button"
+      >
+        Search
+      </button>
+
+      {output && <h2>{output}</h2>}
     </div>
   );
 }
-
-export default App;
